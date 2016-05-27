c0a792764f24b38a22da63d9737b29424d410ce5
qingyu <cqy@umich.edu>
2016-05-28 01:02:55 +0800

    modify sh file on edison board

--- stream.sh
+++ stream.sh
@@ -1,1 +1,1 @@
-/home/root/bin/ffmpeg/ffmpeg -v verbose -r 10 -s 640x480 -f video4linux2 -i /dev/video0 http://192.168.42.20:8000/webcam.ffm
+/home/root/bin/ffmpeg/ffmpeg -v 0 -r 10 -s 640x480 -f video4linux2 -i /dev/video0 http://192.168.1.249:8888/webcam.ffm
